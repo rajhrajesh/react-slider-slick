43f58299c8801f6be5df3d7aa5180086d4e43b92
Rajesh P <penurajeshitsd@gmail.com>
2024-08-10 07:32:01 +0000

    slider

---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,31 @@
+import Slider from 'react-slick'
+
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 3000,
+    cssEase: 'linear',
+  }
+  return (
+    <div className="planets-app-container" data-testid="planets">
+      <h1 className="heading">PLANETS</h1>
+      <Slider {...settings}>
+        {planetsList.map(eachPlanet => (
+          <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
